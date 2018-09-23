56555509771564612fe3febcfd223292236c68c7
Abhyuday <abhyuday964808@gmail.com>
2018-09-24 01:58:50 +0530

    UVA 10360

--- RatAttack.java
+++ RatAttack.java
@@ -10,14 +10,14 @@ class coord
 
 }
 
-class RatAttack{
+class Main{
 	public static void main(String[] args){
 		Scanner in = new Scanner(System.in);
 		
 		int t=in.nextInt();
 		while(t-->0){
 			int[][] a=new int[1025][1025];
-		int[][] killed =new int[1025][1025];
+		int[][] killed =new int[10256][1025];
 			int d= in.nextInt();
 			int n = in.nextInt();
 			coord maxC=null;
@@ -26,14 +26,12 @@ class RatAttack{
 			while(n-->0){
 				int r=in.nextInt();
 				int col= in.nextInt();
-				c.add(new coord(r,col));
+				//c.add(new coord(r,col));
 				int p = in.nextInt();
-				a[r][col]=p;}
-				int s=0;
-			for(int i=0;i<c.size();i++){
-				for(int j= c.get(i).x-d;j<=c.get(i).x+d && j>=0 && j<1024;j++ ){
-					for(int k= c.get(i).y-d;k<=c.get(i).y+d && k>=0 && k<1024;k++ ){
-						killed[j][k]+=a[c.get(i).x][c.get(i).y];
+				a[r][col]=p;
+			for(int j= r-d;j<=r+d && j>=0 && j<1024;j++ ){
+					for(int k= col-d;k<=col+d && k>=0 && k<1024;k++ ){
+						killed[j][k]+=a[r][col];
 					    if(killed[j][k]>maxi){
 					    	maxi =killed[j][k];
 					    	maxC=new coord(j,k);
@@ -43,8 +41,8 @@ class RatAttack{
 					    		maxC=new coord(j,k);
 					    		maxi=killed[j][k];
 					    	}
-					    }
-					}
+					    }}
+				
 				}
 			}	
 	
@@ -52,5 +50,4 @@ class RatAttack{
 
 		
 		}
-	}
-}
+	}}
